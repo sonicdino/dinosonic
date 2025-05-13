a6603646a10bb5261d4d1cd2215007c6e47b40a5
RapidFuge <rapidfugegt1@mgail.com>
2025-05-13 19:20:27 +0800

    Comment out ListenBrainz.ts for now.

--- src/ListenBrainz.ts
+++ src/ListenBrainz.ts
@@ -1,4 +1,4 @@
-import { Song, User } from './zod.ts';
+// import { Song, User } from './zod.ts';
 
-export async function scrobble(user: User, submission: boolean, time: Date, track: Song) {
-}
+// export async function scrobble(user: User, submission: boolean, time: Date, track: Song) {
+// }
